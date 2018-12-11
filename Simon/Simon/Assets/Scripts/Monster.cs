dc1c9606d9cfd8d31f8cdd75965b76e653954c66
sRocky8 <sroccapr@student.fitchburgstate.edu>
2018-12-11 15:20:45 -0500

    Boss Script progress

--- Simon/Simon/Assets/Scripts/Monster.cs
+++ Simon/Simon/Assets/Scripts/Monster.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class Monster : MonoBehaviour {
 
@@ -10,25 +11,51 @@ public class Monster : MonoBehaviour {
 	public Transform playerTransform;
     public GameObject player;
 
-	//Private Variables
+	public float maxNonDashDistance;
+	public float maxAttackDistance;
 
+	public bool attacking;
+	public bool stunned;
+	public bool inHitstun;
+
+	//Private Variables
+	private float distanceToPlayer;
+
+	private bool phaseOne;
+	private bool active;
 
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent> ();
+
+		if (SceneManager.GetSceneByName == "BossPhase 1" || SceneManager.GetSceneByName == "BossPhase 2") {
+			active == true;
+		}
+		else{
+			active = false;
+		}
+
+		if (SceneManager.GetSceneByName == "BossPhase 1") {
+			phaseOne == true;
+		}
+		if (SceneManager.GetSceneByName == "BossPhase 2") {
+			phaseOne == false;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(player != null)
         {
-            float distanceToPlayer = Vector3.Distance(player.transform.position, gameObject.transform.position);
+            distanceToPlayer = Vector3.Distance(player.transform.position, gameObject.transform.position);
             Debug.Log(string.Format("Distance between player and monster is {0}", distanceToPlayer));
         }
 	}
 
 	void FixedUpdate(){
-		Vector3 walkTo = playerTransform.transform.position;
-		agent.destination = walkTo;
+		if (distanceToPlayer <= maxNonDashDistance) {
+			Vector3 walkTo = playerTransform.transform.position;
+			agent.destination = walkTo;
+		}
 	}
 }
