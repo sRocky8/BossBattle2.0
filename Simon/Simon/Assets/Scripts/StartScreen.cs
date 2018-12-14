1198cc01135259ceb20e01e44c8c32226dd3bb6e
sRocky8 <sroccapr@student.fitchburgstate.edu>
2018-12-14 12:10:21 -0500

    Misc.
    Added sound in folders, Start Screen, and button, some other stuff I don't remember.

---
+++ Simon/Simon/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour {
+
+    //Public Variables
+    public Button newGameButton;
+//    public GameObject fade;
+//    public GameObject music;
+    public float timeToWait;
+//    public float volumeDecrease;
+    public bool buttonClicked;
+
+    //Private Variables
+//    private Animator fadeOut;
+//    private AudioSource musicAudio;
+
+    void Start()
+    {
+//        fadeOut = fade.GetComponent<Animator>();
+//        musicAudio = music.GetComponent<AudioSource>();
+        buttonClicked = false;
+//        volumeDecrease = (1.0f / 300.0f);
+    }
+
+    void Update()
+    {
+        if (buttonClicked == true)
+        {
+//            musicAudio.volume -= volumeDecrease;
+        }
+    }
+
+    public void ButtonClicked(string newGame)
+    {
+        buttonClicked = true;
+//        fade.SetActive(true);
+        StartCoroutine(TransitionCoRoutine());
+    }
+
+    private IEnumerator TransitionCoRoutine()
+    {
+//        fadeOut.Play("FadeOut");
+
+        yield return new WaitForSeconds(timeToWait);
+
+        SceneManager.LoadScene(1);
+    }
+}
